452ce116e149d59e9d196d4604f1d3bd5980ec4a
jayascodez <jayabrooks1@gmail.com>
2024-11-27 10:51:19 +0000

    attempting another branch

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -317,3 +317,9 @@ describe("PATCH /api/articles/:article_id", () => {
     })
   });
 });
+
+describe.only("DELETE /api/comments/:comment_id", () => {
+  test("204: Sucessfully deleted comment by comment ID", () => {
+
+  })
+})
